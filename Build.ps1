80cb901931a4da00a3a87a5a5fd6e785f61d22a4
Jehoschua <SmartGit@jig.ch>
2018-04-11 11:00:27 +0200

    Build.ps1 - Tried to add a Script PowerShell Description (.Synopsis, .Description, etc.) - Script param():   - Fixed the Position Numbers (based on ParameterSet)   - Parameter InstallAndTestModule:     - Added the Alias InstallAndImportModule because InstallAndTestModule does not Test the Module but it calls Import-Module. Therefore, this is more precise.   - New Parameter: IncrementVersion     ValidateSet: Major, Minor, Patch, Build     The Argument defines which part of a semantic versioning (see: https://semver.org/)     should be automatically incremented:       Argument      <Major>.<Minor>.<Patch>.<Build>       Major           ++       0       0      0       Minor          Keep     ++       0      0       Patch          Keep    Keep     ++      0       Build          Keep    Keep    Keep    ++   - New Parameter: ShowVersion     I added this switch *and* a build task ShowVersion so that the version number can be displayed.   - New Parameter: ForceInstallModule     Maybe, we could use -Force 	 The idea: It is used to allow the Build Process to install any necessary Module without asking the user. This is useful to fire and forget the Build task, especially if it will be used on a Build Server :-) - I have testet ModuleBuild using PSScriptAnalyzer and therefore I have added one or more [Diagnostics.CodeAnalysis.SuppressMessageAttribute] statements

--- Build.ps1
+++ Build.ps1
@@ -1,18 +1,95 @@
 #Requires -Version 5
+
+<#
+    .SYNOPSIS
+        Invokes the process to build a PowerShell Module
+
+    .DESCRIPTION
+	    This Script belongs to the PowerShell Module 'ModuleBuild' from Zachary Loeber (zloeber), see https://github.com/zloeber/ModuleBuild
+
+	    The Build Tasks are defined in <MyModuleName>.build.ps1
+    
+    .PARAMETER BuildModule
+       	Invokes the default build Task defined by a dot (task . …) in <MyModuleName>.build.ps1
+    
+    .PARAMETER UploadPSGallery
+    	Invokes this command: Invoke-Build -Task PublishPSGallery
+	    It pushes the current release to the PS Script Gallery:
+	    Gallery: https://www.powershellgallery.com/
+	    Man:     https://docs.microsoft.com/en-us/powershell/gallery/readme
+    
+    .PARAMETER InstallAndTestModule
+    	Invokes this command: Invoke-Build -Task InstallAndTestModule
+    	Assigned main tasks:
+    	- InstallModule: Install the new built module to the local machine
+    	- TestInstalledModule: Calls Import-Module for the new built module
+    
+    .PARAMETER NewVersion
+	    Invokes this command: Invoke-Build -Task UpdateVersion -NewVersion $NewVersion -ReleaseNotes $ReleaseNotes
+	    Assigned main tasks:
+	    - NewVersion: Set the new version to the module
+	    - UpdateRelease: Updates the current module manifest with the ReleaseNotes 
+                         and the version defined in the build config file (if they differ)
+    
+    .PARAMETER IncrementVersion
+        ValidateSet: Major, Minor, Patch, Build
+        This Parameter defines which part of a semantic versioning (see: https://semver.org/)
+        should be automatically incremented: 
+
+        Argument      <Major>.<Minor>.<Patch>.<Build>
+        Major           ++       0       0      0
+        Minor          Keep     ++       0      0
+        Patch          Keep    Keep     ++      0
+        Build          Keep    Keep    Keep    ++
+    
+    .PARAMETER ShowVersion
+        Invokes this command: Invoke-Build -Task ShowVersion
+    
+    .PARAMETER ReleaseNotes
+    	Invokes this command: Invoke-Build -Task UpdateVersion -NewVersion $NewVersion -ReleaseNotes $ReleaseNotes
+    	Assigned main tasks: See Parameter NewVersion
+
+    .PARAMETER InsertCBH
+    	Invokes this command: Invoke-Build -Task InsertMissingCBH
+    	Assigned main tasks:
+    	- UpdateCBHtoScratch: Update public functions to include a template comment based help.
+    
+    .PARAMETER ForceInstallModule
+	    If a 3rd party Module need to be installed, then Install-Module will use -Force
+
+    .EXAMPLE
+        PS C:\> .\Build.ps1
+        Builds the Module in the current directory
+
+    .NOTES
+    	Original Conecpt & Idea: Zachary Loeber (zloeber)
+    	ModuleBuild, see https://github.com/zloeber/ModuleBuild
+	    A scaffolding framework which can be used to kickstart a generic PowerShell module project.
+#>
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseCmdletCorrectly', '')]
 [CmdletBinding(DefaultParameterSetName = 'Build')]
 param (
     [parameter(Position = 0, ParameterSetName = 'Build')]
     [switch]$BuildModule,
-    [parameter(Position = 2, ParameterSetName = 'Build')]
+    [parameter(Position = 1, ParameterSetName = 'Build')]
     [switch]$UploadPSGallery,
-    [parameter(Position = 3, ParameterSetName = 'Build')]
+    [parameter(Position = 2, ParameterSetName = 'Build')]
+    [Alias(InstallAndImportModule)]
     [switch]$InstallAndTestModule,
-    [parameter(Position = 4, ParameterSetName = 'Build')]
+    [parameter(Position = 3, ParameterSetName = 'Build')]
     [version]$NewVersion,
+    [parameter(Position = 4, ParameterSetName = 'Build')]
+    [ValidateSet('Major', 'Minor', 'Patch', 'Build', IgnoreCase)]
+    [string]$IncrementVersion,
+    [parameter(ParameterSetName = 'Build')]
+    [switch]$ShowVersion,
     [parameter(Position = 5, ParameterSetName = 'Build')]
     [string]$ReleaseNotes,
-    [parameter(Position = 6, ParameterSetName = 'CBH')]
-    [switch]$InsertCBH
+    [parameter(Position = 0, ParameterSetName = 'CBH')]
+    [switch]$InsertCBH,
+    [parameter(Position = 6, ParameterSetName = 'Build')]
+    [parameter(Position = 1, ParameterSetName = 'CBH')]
+    [switch]$ForceInstallModule
 )
 
 function PrerequisitesLoaded {
@@ -20,7 +97,8 @@ function PrerequisitesLoaded {
     try {
         if ((get-module InvokeBuild -ListAvailable) -eq $null) {
             Write-Output "Attempting to install the InvokeBuild module..."
-            $null = Install-Module InvokeBuild -Scope:CurrentUser
+            $Splat = @{ Force = ($script:ForceInstallModule.IsPresent -and $script:ForceInstallModule) }
+            $null = Install-Module InvokeBuild -Scope:CurrentUser @Splat
         }
         if (get-module InvokeBuild -ListAvailable) {
             Write-Output -NoNewLine "Importing InvokeBuild module"
@@ -38,6 +116,8 @@ function PrerequisitesLoaded {
 }
 
 function CleanUp {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingEmptyCatchBlock', '')]
+    Param ()
     try {
         Write-Output ''
         Write-Output 'Attempting to clean up the session (loaded modules and such)...'
@@ -73,6 +153,20 @@ switch ($psCmdlet.ParameterSetName) {
                 throw $_
             }
         }
+        
+        If ($NewVersion -ne $null -or
+            $IncrementVersion -ne $null) {
+            Try {
+                Invoke-Build -Task UpdateVersion -NewVersion $NewVersion -ReleaseNotes $ReleaseNotes -IncrementVersion:$IncrementVersion
+            } Catch {
+                Throw $_
+            }
+        }
+        
+        If ($ShowVersion) {
+            Invoke-Build -Task ShowVersion
+        }
+        
         # If no parameters were specified or the build action was manually specified then kick off a standard build
         if (($psboundparameters.count -eq 0) -or ($BuildModule)) {
             try {
