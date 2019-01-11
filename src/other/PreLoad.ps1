9498936e1b6f17c5c5ca111438af8234d28b5127
justin-p <justin-p@users.noreply.github.com>
2019-01-11 17:49:49 +0100

    PSUseDeclaredVarsMoreThanAssignments supress

--- src/other/PreLoad.ps1
+++ src/other/PreLoad.ps1
@@ -1,1 +1,8 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "ModuleBuildLoggingEnabled")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "BuildPath")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "functionpredicate")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "CommonParams")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "ScriptText")]
+param()
+
 $ModuleBuildLoggingEnabled = $false     # Set to true to enable nlog-based logging
