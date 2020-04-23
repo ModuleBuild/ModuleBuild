de7d92ff605236a0200ca87e12917f0617bf0ef5
Justin-p <justin-p@users.noreply.github.com>
2020-04-23 22:24:33 +0200

    remove file copy

--- plugins/plaster/template/scaffold/plugins/NLog/Load copy.ps1
+++
@@ -1,2 +0,0 @@
-Import-Module (Join-Path $MyModulePath 'plugins\nlog\nlogmodule\0.0.2\nlogmodule.psd1') -Force -Scope:Global
-Register-NLog -FileName (Join-Path $ENV:TEMP '<%=$PLASTER_PARAM_ModuleName%>.log') -LoggerName '<%=$PLASTER_PARAM_ModuleName%>'
